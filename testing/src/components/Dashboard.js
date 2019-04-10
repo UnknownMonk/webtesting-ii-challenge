81cfba7ce2ec3e3d8521f3f953591bee74ef094f
UnknownMonk <smodcartoon@gmail.com>
2019-04-10 08:09:56 -0400

    Dashboard test working

--- testing/src/components/Dashboard.js
+++ testing/src/components/Dashboard.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Dashboard = props => {
   return (
     <div>
-      <button onClick={props.strike}>Strike</button>
-      <button onClick={props.ball}>Ball</button>
-      <button onClick={props.foul}>Foul</button>
-      <button onClick={props.hit}>Hit</button>
+      <button data-testid="strike" onClick={props.strike}>Strike</button>
+      <button data-testid="ball" onClick={props.ball}>Ball</button>
+      <button data-testid="foul" onClick={props.foul}>Foul</button>
+      <button data-testid="hit" onClick={props.hit}>Hit</button>
     </div>
   );
 };
