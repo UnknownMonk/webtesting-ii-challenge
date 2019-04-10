691e6669a20a0ab47ca6fac30cebd37cacd05a54
UnknownMonk <smodcartoon@gmail.com>
2019-04-10 08:44:32 -0400

    display tests working

--- testing/src/components/Display.test.js
+++ testing/src/components/Display.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Display from './Display';
-import { render } from 'react-testing-library';
+import { render, getByText } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 describe('<Display/>', () => {
   it('should render the display component', () => {
+    render(<Display />);
     const testState = {
       balls: 0,
       strikes: 0
@@ -13,6 +14,7 @@ describe('<Display/>', () => {
 
     const balls = getByTestId(/balls/i);
     const strikes = getByTestId(/strikes/i);
+
     expect(balls).toHaveTextContent(/Balls/i);
     expect(strikes).toHaveTextContent(/Strikes/i);
   });
